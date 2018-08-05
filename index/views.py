5f3e85299e3e7ccb4b7a2bcdbb01c749c0dcf408
Saurabh_Dube <dube.saurabh5@gmail.com>
2018-08-05 17:29:53 +0530

    1st commit..basic upload

---
+++ index/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def Form(request):
+    return render(request,"index/forms.html",{})
+def Upload(request):
+    for count,x in enumerate(request.FILES.getlist("files")):
+        def process(f):
+            with open('C:\Users\Saurabh_Dube\PycharmProjects\FileUpload\media'+'\\file_'+str(count), 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+        process(x)
+    return HttpResponse("Files Uploaded on %s"% MEDIA_ROOT)
+
+# Create your views here.
